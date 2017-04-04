ce36fa701ac3970156e3333f0e10b7232f079552
Matthew Moyer <matthewrmoyer@gmail.com>
2017-04-04 11:55:44 -0600

    get authors and and author ids inside of book object to full data array

--- public/scripts/books.js
+++ public/scripts/books.js
@@ -1,14 +1,55 @@
 $(document).ready(function() {
 	$(".get-books-button").on('click', () => {
 		console.log('lcikadf')
+
+
+		var fullData = []
+
 		$.get('https://galvanize-reads-moyer.herokuapp.com/books', (data) => {
+			console.log("book table")
 			console.log(data)
 			var source = $("#book-template").html()
 			var template = Handlebars.compile(source)
 			data.forEach((element) => {
+				fullData.push(element)
 				var html = template(element)
 				$('.books-placeholder').append(html)
 			})
+
+
+			$.get('https://galvanize-reads-moyer.herokuapp.com/author_book', (data) => {
+				console.log("join table")
+				console.log(data)
+				data.forEach((element)=> {
+					var authorId = element.author_id
+					var bookId = element.book_id
+
+					console.log("Author Id: " + authorId)
+					console.log("Book Id: " + bookId)
+
+
+					fullData.forEach((fullDataElement) => {
+						if(fullDataElement.id == bookId){
+							if(typeof fullDataElement.author_id != 'object'){
+								fullDataElement.author_id = []
+							}
+							fullDataElement.author_id.push(authorId)
+						}
+					})
+				})
+			})
+
+
+			$.get('https://galvanize-reads-moyer.herokuapp.com/authors', (data) => {
+				console.log("author table")
+
+				console.log(data)
+
+			})
 		})
+
+		console.log("Full Data")
+			console.log(fullData)
 	})
+
 });
