f3dbe15ad288a72b329c47bc4af8d3a2828e7829
Matthew Moyer <matthewrmoyer@gmail.com>
2017-04-04 12:47:02 -0600

    add author to books.html

--- public/scripts/books.js
+++ public/scripts/books.js
@@ -6,31 +6,22 @@ $(document).ready(function() {
 		var fullData = []
 
 		$.get('https://galvanize-reads-moyer.herokuapp.com/books', (data) => {
-			console.log("book table")
-			console.log(data)
-			var source = $("#book-template").html()
-			var template = Handlebars.compile(source)
 			data.forEach((element) => {
 				fullData.push(element)
-				var html = template(element)
-				$('.books-placeholder').append(html)
 			})
 
 
 			$.get('https://galvanize-reads-moyer.herokuapp.com/author_book', (data) => {
-				console.log("join table")
-				console.log(data)
-				data.forEach((element)=> {
+
+				data.forEach((element) => {
 					var authorId = element.author_id
 					var bookId = element.book_id
 
-					console.log("Author Id: " + authorId)
-					console.log("Book Id: " + bookId)
 
 
 					fullData.forEach((fullDataElement) => {
-						if(fullDataElement.id == bookId){
-							if(typeof fullDataElement.author_id != 'object'){
+						if (fullDataElement.id == bookId) {
+							if (typeof fullDataElement.author_id != 'object') {
 								fullDataElement.author_id = []
 							}
 							fullDataElement.author_id.push(authorId)
@@ -41,15 +32,37 @@ $(document).ready(function() {
 
 
 			$.get('https://galvanize-reads-moyer.herokuapp.com/authors', (data) => {
-				console.log("author table")
 
-				console.log(data)
 
+				data.forEach((element) => {
+					var author = {
+						author_id: element.id,
+						first_name: element.first_name,
+						last_name: element.last_name
+					}
+
+					fullData.forEach((fullDataElement) => {
+						if (fullDataElement.author_id == author.author_id) {
+							fullDataElement.author = author
+						}
+					})
+				})
+
+
+				console.log("Full Data")
+		console.log(fullData)
+
+		var source = $("#book-template").html()
+		var template = Handlebars.compile(source)
+		fullData.forEach((element) => {
+			var html = template(element)
+			$('.books-placeholder').append(html)
+		})
 			})
+
+
 		})
 
-		console.log("Full Data")
-			console.log(fullData)
 	})
 
 });
